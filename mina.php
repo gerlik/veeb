c98c4e04d332bb40561daa48604140298abd25f7
Gerli <gerli.koiv@khk.ee>
2017-12-11 10:15:47 +0200

    defineeritud ja väljastatud eesnimi

---
+++ mina.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IBM
+ * Date: 11/12/2017
+ * Time: 09:07
+ * Ülesanne: loo muutuja enda nimega, väärtuseks sõne, oma eesnimi, kasuta mõnda html tagi
+ */
+
+$gerli = 'gerli';
+echo 'Minu nimi on '. $gerli .'.<br />';
+print 'Minu nimi on '. $gerli .'.<br />';
+print('Minu nimi on  $gerli<br/>');
+
+
+?>
