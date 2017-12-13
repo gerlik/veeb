6b0e09ad87b1d59e618a9636223bf163a53fd954
Gerli <gerli.koiv@khk.ee>
2017-12-13 14:40:13 +0200

    while-tsükli põhistruktuur

--- tsyklid.php
+++ tsyklid.php
@@ -6,15 +6,19 @@
  * Time: 14:12
  */
 for ($i = 1; $kord <= 10; $kord++){
-    if($kord % 2 ==0){
+    if($kord % 2 == 0){
         $varv = 'red';
+        break;
     }else {
         $varv = 'blue';
     }
-    //värvi defineerimine
-//    if($varv == 'blue'){
-//        continue;
-//    }
     echo '<font color="'.$varv.'">'.$kord.'</font><br />';
-    //echo $kord.'<br />';
+}
+
+echo '<br />';
+$kord = 1;
+while ($kord <= 10){
+    echo '<i>'.$kord.'</i><br />';
+    $kord++;
+
 }
