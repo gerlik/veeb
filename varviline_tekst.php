05a2f4ad403036d7ac22d42526cca31ffa0ed176
Gerli <gerli.koiv@khk.ee>
2017-12-13 15:11:26 +0200

    tekstivärvi muutmine heksakoodis

--- varviline_tekst.php
+++ varviline_tekst.php
@@ -7,12 +7,13 @@
  */
 
 //värviline tekst, isevärskendav leht
-header("Refresh:2");
+header("Refresh:1");
 for($rida = 1; $rida <=5; $rida++){
     $varv = '#';
     for ($kord = 1; $kord <=6; $kord++){
-        $juhuarv =  rand(0, 9);
-        $varv = $varv.$juhuarv;
+        $juhuarv =  rand(0, 15);
+        $juhuhex = dechex($juhuarv);
+        $varv = $varv.$juhuhex;
         //echo $varv.'<br />';
     }
     echo '<font color ="'.$varv.'">Värviline tekst</font><br/>';
