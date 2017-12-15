7f2bb9c0deedd259d6214d065810d9f92ee1a650
Gerli <gerli.koiv@khk.ee>
2017-12-15 09:08:08 +0200

    funktsioon tabeli loomiseks, täidetud arvudega 10-99

--- funktsioonid.php
+++ funktsioonid.php
@@ -9,11 +9,11 @@
 
 //tabeli loomine, parameetrid: ridade arv, veergude arv
 //sisu: juhuslikud arvud, vahemik 10-99
-function looTabel($ridadeArv,$veergudearv){
+function looTabel($ridadeArv,$veergudeArv){
     echo '<table border="1">';
-    for ($reanr = 1; $reanr <= $ridadeArv; $reanr++){
+    for ($reaNr = 1; $reaNr <= $ridadeArv; $reaNr++){
         echo '<tr>';
-        for ($veerunr = 1; $veerunr <= $veergudearv; $veerunr++){
+        for ($veeruNr = 1; $veeruNr <= $veergudeArv; $veeruNr++){
             echo '<td>';
             echo rand(10,99);
             echo '</td>';
