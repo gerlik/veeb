fffaacb309edcf48feae4202b9f8ebd6dc5c1c60
Gerli <gerli.koiv@khk.ee>
2017-12-18 13:12:05 +0200

    lisatud funktsioon, mis leiab mitu korda esineb kindel arv suvalises arvus

--- funktsioonid2.php
+++ funktsioonid2.php
@@ -21,3 +21,22 @@ for ($kord = 1; $kord <= 5; $kord++){
     $number = rand(11,1795);
     echo 'Arvu '.$number.' ristsumma on '.arvuSumma($number).'<br />';
 }
+echo '<hr>';
+
+//funktsioon otsiNumber, parameetriks suvaline arv, kindel arv, mitu korda esineb kindel arv suvalises arvus
+function otsiNumber($suvalineArv, $kindelArv){
+    echo $kindelArv.' esineb numbris '.$suvalineArv;
+    $luger = 0;
+    while ($suvalineArv != 0){
+        $arv = $suvalineArv % 10;
+        if ($arv == $kindelArv){
+            $luger++;
+        }
+        $suvalineArv = $suvalineArv /10;
+        settype($suvalineArv, 'integer');
+    }
+    echo ' '.$luger.' korda'.'<br />';
+}
+otsiNumber(15151, 5);
+otsiNumber(15151, 1);
+otsiNumber(15151, 7);
