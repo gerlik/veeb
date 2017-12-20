7d2bd70538eb8d89cfaf30055ad8245b3f44978c
Gerli <gerli.koiv@khk.ee>
2017-12-20 12:09:10 +0200

    loodud testmassiiv, testväljastus

---
+++ massiivid.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gerli
+ * Date: 20/12/2017
+ * Time: 12:02
+ */
+
+$arvud = array(1, 5, 15 ,15, 1, 51);
+var_dump($arvud);
+echo '<hr />';
+echo '<pre>';
+print_r($arvud);
+echo '<pre />';
