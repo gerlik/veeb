384b1227b6baffb75cee1502d017392f5e37b6ea
Gerli <gerli.koiv@khk.ee>
2017-12-20 14:19:16 +0200

    tehtud ülesanded 3, 4, 5

--- massiivid2.php
+++ massiivid2.php
@@ -56,5 +56,89 @@ function valjastaMassiiv($massiiv){
     }
     echo '</table>';
 }
-$massiiv = array(1,2,3,4,5,6,7,7);
+$massiiv = array(1,2,3,4,5);
 valjastaMassiiv($massiiv);
+echo '<hr />';
+
+/*
+ * 3.
+ * Loo funktsioon nimega vahetaMinMax, mis võtab
+ * parameetrina täisarvude massiivi (testimises
+ * võib kasutada looMassiiv funktsiooniga
+ * genereeritud massiiv)
+ * ja leiab kõige väiksema ja kõige suurema
+ * elemendi antud massiivis ning vahetab nende asukohad.
+ *
+ * Kontrolli kas kohad on vahetatud kasutades
+ * valjastaMassiiv funktsiooni
+*/
+
+function vahetaMinMax($arvud){
+    $min = min($arvud);
+    $max = max($arvud);
+    valjastaMassiiv($arvud);
+    echo 'Miinimum = '.$min.'<br/>';
+    echo 'Mmaksimum = '.$max.'<br />';
+    for ($i = 0; $i < count($arvud); $i++){
+            if ($arvud[$i] == $min){
+                $arvud[$i] = $max;
+            }else if($arvud[$i] == $max){
+                $arvud[$i] = $min;
+            }
+        }
+        valjastaMassiiv($arvud);
+}
+$array = array(213,141,41,1,51);
+vahetaMinMax($array);
+echo '<hr />';
+
+/*
+ * 4.
+ * Loo funktsioon nimega elementideKorrutis, mis
+ * võtab parameetrina täisarvude massiivi ja leiab
+ * korrutis nendest arvudest, mis on suurem kui 0
+ * ja mille indeksid on paarisarvud. Tulemus väljastatakse
+ * antud funktsiooni abil kujul:
+ *
+ * Massiiv on 1, 0, 6, 0, 0, 3, 5
+ * Tulemus: 1 * 6 * 5 = 30
+*/
+
+function elementideKorrutis($massiiv){
+    $korrutis = 1;
+    $i = 0;
+    foreach ($massiiv as $item){
+        $i++;
+        if ($item > 0 and $massiiv[$i]%2 ==0){
+            $korrutis = $korrutis * $item;
+
+    }
+    }echo $korrutis;
+}
+$massiiv = array(1, 0, 6, 0, 0, 3, 5);
+elementideKorrutis($massiiv);
+echo '<hr />';
+
+/*
+ * 5.
+ * Töötluses kasutada massiiv
+ * $massiiv = array(1, 1, 1, 2, 2, 2, 2, 3).
+ * Loo funktsioon mitteDubleeri, mis võtab
+ * parameetrina massiivi ja ei väljasta dubleeritud
+ * elemendid. Ehk näidemassiivi puhul tuleb väljastada
+ * ainult 1, 2, 3 - s.t. ilma dubleerimist.
+ * NB! Lahenduses tuleb kasutada AINULT ÜKS foreach
+ * tsükli abil, samuti EI SAA kasutada
+ * grupeerimisfunktisoone ega muuta antud massiivi
+ * */
+
+function mitteDubleeri($massiiv){
+    //$notDuubel = [];
+    foreach (array_unique($massiiv) as $item){
+            $notDuubel  = $item;
+            echo $notDuubel;
+    }
+}
+$massiiv2 = array(1, 1, 1, 2, 2, 2, 2, 3);
+mitteDubleeri($massiiv2);
+
