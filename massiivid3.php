6f00760746e7e924531bc409fbade3e402d38acd
Gerli <gerli.koiv@khk.ee>
2018-01-08 08:51:38 +0200

    massiiv

---
+++ massiivid3.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gerli
+ * Date: 08/01/2018
+ * Time: 08:49
+ */
+
+$opilane = array(
+    'eesnimi' => 'Mart',
+    'perenimi' => 'Lepp',
+    'vanus' => 16,
+    'klass' => 10,
+);
