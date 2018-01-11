58f392f65a494797cab5a3db5ae3de71d0035e18
Gerli <gerli.koiv@khk.ee>
2018-01-11 12:57:17 +0200

    andmebaasi sisestamine

--- andmebaasi.php
+++ andmebaasi.php
@@ -8,8 +8,8 @@
 
 error_reporting(E_ALL);
 
-$yhendus = mysql_connect('localhost', 'gerlikoiviktkhk',
-    'UUR}QPTe}pco', 'gerlikoi_ABloeng');
+$yhendus = mysqli_connect('localhost', 'usr',
+    'password', 'db');
 
 if(!$yhendus){
     echo 'Probleem ühendusega';
@@ -17,6 +17,31 @@ if(!$yhendus){
     echo mysqli_connect_errno($yhendus).'<br />';
 }else{
     echo 'On loodud ühendus<br />';
+    $eesnimi = 'Test';
     $sql = 'INSERT INTO kasutajad(eesnimi, perenimi, synnikuupaev)'.
-        'VALUES(\'Test\', \'Kasutaja\', \'1999-03-08\')';
+        'VALUES(\''.$eesnimi.'\', \'Kasutaja\', \'1999-03-08\')';
+    $tulemus = mysqli_query($yhendus, $sql);
+    if(!$tulemus){
+        echo 'Probleem päringuga<br />';
+        echo mysqli_error($yhendus).'<br />';
+        echo mysqli_errno($yhendus).'<br />';
+    }else{
+        echo 'Andmed on salvestatud<br />';
+        $sql = 'SELECT * FROM kasutajad';
+        if (!$tulemus){
+            echo 'Probleem päringuga';
+            echo mysqli_connect_error($yhendus).'<br />';
+            echo mysqli_connect_errno($yhendus).'<br />';
+        }else{
+            echo '<table border = "1">';
+            while ($rida = mysqli_fetch_assoc($tulemus)){
+                echo '<tr>';
+                foreach ($rida as $element){
+                    echo '<td>'.$element.'</td>';
+                }
+                echo '</tr>';
+            }
+            echo '</table>';
+        }
+    }
 }
