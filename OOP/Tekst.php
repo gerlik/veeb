40fe51dcb2bc09cff554c83107456df145575b6d
Gerli <gerli.koiv@khk.ee>
2018-01-15 08:57:09 +0200

    defineeritud klass ja klassimuutuja

---
+++ OOP/Tekst.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IBM
+ * Date: 15/01/2018
+ * Time: 08:52
+ */
+
+class Tekst
+{
+    //klassi omadus(ed)
+    //class variables
+    var $sone = '';
+}
